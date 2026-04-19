b6f80f19eb22521f62772069e29220b079c2e202
TheBeardedWonder <the@beardedwonder.com>
2026-04-19 09:45:25 -0700

    bookmark/favorite upgrades:

--- Sex Stories/Sex Stories/ReaderBookmark.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct ReaderBookmark: Identifiable, Codable, Hashable {
-    let id: String
-    let storyID: String
-    let anchorID: String
-    let title: String
-    let createdAt: Date
-}
